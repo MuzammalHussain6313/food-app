bd0f0680a8e9d0182fe40a338fbccac00b11c212
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-04-22 19:41:17 +0500

    List of users updated.

--- src/app/admin/list/list.module.ts
+++ src/app/admin/list/list.module.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 import { ListPage } from './list.page';
+import {ReviewComponent} from '../../charityHouse/donner-list/review/review.component';
 import {PopoverComponent} from '../popover/popover.component';
 
 @NgModule({
--- src/app/admin/list/list.page.ts
+++ src/app/admin/list/list.page.ts
@@ -1,58 +1,102 @@
-import { Component, OnInit } from '@angular/core';
-import { ListService } from '../../list.service';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { PopoverController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {ListService} from '../../list.service';
+import {Router} from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {HttpClient, HttpResponse, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PopoverController} from '@ionic/angular';
 import {PopoverComponent} from '../popover/popover.component';
+import {Storage} from '@ionic/storage';
+import 'rxjs-compat/add/operator/map';
+import 'rxjs-compat/add/operator/catch';
 
 @Component({
-  selector: 'app-list',
-  templateUrl: 'list.page.html',
-  styleUrls: ['list.page.scss']
+    selector: 'app-list',
+    templateUrl: 'list.page.html',
+    styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  constructor(public router: Router,
-              public http: HttpClient,
-              public popoverController: PopoverController) {
-    this.data = this.http.get('http://localhost:8095/users/list');
-    this.data.subscribe(data => {
-      this.result = data.content;
-    });
-    console.log('result' + this.result);
-  }
+    t;
+    constructor(public router: Router,
+                private storage: Storage,
+                public http: HttpClient,
+                public popoverController: PopoverController) {
+        this.http.get('http://localhost:8095/users/list',
+            {observe: 'response'}).subscribe(response => {
+            if (response.status === 200 || response.status === 201) {
+                this.t = response.body;
+                console.log('data loading from API');
+                this.result = this.t.content;
+                this.storage.clear();
+                this.storage.set('users', this.result);
+                console.log('t : ', this.t.content);
+            }
+            // You can access status:
+            console.log('status code', response.status);
+            console.log('complete content', response.body);
+            // Or any other header:
+            console.log('X-Custom-Header', response.headers.get('X-Custom-Header'));
+        }, (error) => {
+            console.log('data loading from loadData function.');
+            this.loadData();
+            console.log('error', error);
+        });
+        // this.data = this.http.get('http://localhost:8095/users/list');
+        // this.data.subscribe(data => {
+        //   // if (data.status === 200) {
+        //   console.log('code', data.status);
+        //   console.log('data', this.data);
+        //   this.result = data.content;
+        // //   this.storage.set('users', this.result);
+        //   // } else {
+        //   //   this.loadData();
+        //   // }
+        //  });
+        // // if (!this.result) {
+        // //   this.loadData();
+        // // } else if (this.result) {
+        // //   this.storage.set('users', this.result);
+        // // }
+        // this.loadData();
+        console.log('result' + this.result);
+    }
+    result: any = [];
+    data: Observable<any>;
 
-  // test
-  result: any = [];
-  data: Observable<any>;
-  ngOnInit(): void {
-  }
+    loadData() {
+        this.storage.get('users').then((val) => {
+            this.result = val;
+            console.log('Your age is', val);
+        });
+    }
 
-  addUser() {
-    this.router.navigate(['addUser']);
-  }
+    ngOnInit(): void {
+    }
 
-  showSingleItem(item: any) {
-    // const id = '{' +
-    //     '"id": 1,' +
-    //     '"streetAddress": "Home 2 steet 1",' +
-    //     '"city": "Lahore",' +
-    //     '"zipCode": 54500,' +
-    //     '"state": "punjab",' +
-    //     '"country": "Pakistan"' +
-    //     '}';
-    const id = item.id;
-    const url = `list/${id}`;
-    console.log(url);
-    this.router.navigateByUrl(url);
-  }
+    addUser() {
+        this.router.navigate(['addUser']);
+    }
 
-  async presentPopover(myEvent, item: any) {
-    const popover = await this.popoverController.create({
-      component: PopoverComponent,
-      componentProps: {id: item.id }
-    });
-    return await popover.present();
-  }
+    showSingleItem(item: any) {
+        // const id = '{' +
+        //     '"id": 1,' +
+        //     '"streetAddress": "Home 2 steet 1",' +
+        //     '"city": "Lahore",' +
+        //     '"zipCode": 54500,' +
+        //     '"state": "punjab",' +
+        //     '"country": "Pakistan"' +
+        //     '}';
+        const id = item.id;
+        const url = `list/${id}`;
+        console.log(url);
+        this.router.navigateByUrl(url);
+    }
+
+    async presentPopover(myEvent, item: any) {
+        const popover = await this.popoverController.create({
+            component: PopoverComponent,
+            componentProps: {id: item.id}
+        });
+        return await popover.present();
+    }
 }
